e36f65181a40f5279e009cca3a12b9bd4b3df6b1
vAndrean-drag <andrean.sihombing83@gmail.com>
2024-06-16 18:56:13 +0800

    fix login for the last time

--- frontend/src/context/book-context.jsx
+++ frontend/src/context/book-context.jsx
@@ -14,11 +14,13 @@ const defaultVal = {
 export const BookContext = createContext(defaultVal);
 export const BookContextProvider = (props) => {
   const [cookies, setCookies] = useCookies(["access_token"]);
-  const [isAuthenticated, setAuthenticated] = useState(cookies.access_token !== undefined);
-  console.log("auth " + isAuthenticated);
-  console.log("access_token " + cookies.access_token);
+  const [isAuthenticated, setAuthenticated] = useState(cookies.access_token === undefined);
+  console.log("auth " + isAuthenticated + " access_token " + cookies.access_token);
+  // console.log(cookies.access_token !== undefined);
+
   useEffect(() => {
     if (!isAuthenticated) {
+      console.log("masuk");
       localStorage.clear();
       setCookies("access_token", undefined);
     }
