6d03491c9634327efd36888d1e7f60e8fe3fbd55
vAndrean-drag <andrean.sihombing83@gmail.com>
2024-06-16 10:24:17 +0800

    fix login2

--- frontend/src/context/book-context.jsx
+++ frontend/src/context/book-context.jsx
@@ -15,6 +15,8 @@ export const BookContext = createContext(defaultVal);
 export const BookContextProvider = (props) => {
   const [cookies, setCookies] = useCookies(["access_token"]);
   const [isAuthenticated, setAuthenticated] = useState(cookies.access_token === undefined);
+  console.log("auth " + isAuthenticated);
+  console.log("access_token " + cookies.access_token);
   useEffect(() => {
     if (!isAuthenticated) {
       localStorage.clear();
